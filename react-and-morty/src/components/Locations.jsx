63ff38861c144cc104b12c2da1e860dae685b590
Pityken <pityken@gmail.com>
2022-09-15 14:11:57 +0200

    Add back to top button

--- react-and-morty/src/components/Locations.jsx
+++ react-and-morty/src/components/Locations.jsx
@@ -2,14 +2,26 @@ import React, { useState, useEffect } from 'react'
 import { useLocations } from "../api/useData";
 import { LocationsPagination } from './LocationsPagination'
 import { Location } from './Location'
+import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
 import CircularProgress from "@mui/material/CircularProgress";
+import { CSSTransition } from "react-transition-group";
+
 import "../App.css";
 import "./Locations.css";
+
+const bactToTopStyles = {
+    position: "fixed",
+    bottom: "2%",
+    right: "2%",
+    fontSize: "5em",
+    cursor: "pointer",
+};
 
 const Locations = () => {
     const [LocationList, setLocationList] = useState([]);
     const [page, setPage] = useState(1);
     const locations = useLocations(page);
+    const [hasScrolled, setHasScrolled] = useState(false);
     const [loading, setLoading] = useState(true);
     const [pageCount, setPageCount] = useState(0)
 
@@ -23,8 +35,28 @@ const Locations = () => {
         }
     }, [locations]);
 
+    useEffect(() => {
+        window.addEventListener("scroll", () => scrollFunction());
+    }, []);
+
     const handlePageChange = (event, value) => {
         setPage(value);
+    };
+
+    const scrollFunction = () => {
+        if (
+            document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20
+        ) {
+            setHasScrolled(true);
+        } else {
+            setHasScrolled(false);
+        }
+    };
+
+    const backToTop = () => {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
     };
 
     return (
@@ -46,6 +78,19 @@ const Locations = () => {
                     ))}
                 </div>
             )}
+            <CSSTransition
+                in={hasScrolled}
+                timeout={500}
+                mountOnEnter
+                unmountOnExit
+                classNames="back-to-top"
+            >
+                <ArrowCircleUpIcon
+                    color="secondary"
+                    onClick={backToTop}
+                    sx={bactToTopStyles}
+                />
+            </CSSTransition>
         </>
     )
 }
